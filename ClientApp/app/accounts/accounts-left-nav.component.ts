610d5879c238a4e5224ecc5b4642f84516b281cf
Quinntyne Brown <quinntynebrown@gmail.com>
2017-08-10 00:14:44 -0400

    left-nav

---
+++ ClientApp/app/accounts/accounts-left-nav.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    templateUrl: "./accounts-left-nav.component.html",
+    styleUrls: [
+        "../../styles/left-nav.css",
+        "./accounts-left-nav.component.css"
+    ],
+    selector: "ce-accounts-left-nav"
+})
+export class AccountsLeftNavComponent { }
