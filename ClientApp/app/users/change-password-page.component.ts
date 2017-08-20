f71594489aadd00e6f647f8d53b392fbd3e54e82
Quinntyne Brown <quinntynebrown@gmail.com>
2017-08-20 10:10:23 -0400

    change password form

---
+++ ClientApp/app/users/change-password-page.component.ts
@@ -0,0 +1,8 @@
+import {Component} from "@angular/core";
+
+@Component({
+    templateUrl: "./change-password-page.component.html",
+    styleUrls: ["./change-password-page.component.css"],
+    selector: "ce-change-password-page"
+})
+export class ChangePasswordPageComponent { }
---
+++ ClientApp/app/users/change-password.component.ts
@@ -0,0 +1,8 @@
+import {Component} from "@angular/core";
+
+@Component({
+    templateUrl: "./change-password.component.html",
+    styleUrls: ["./change-password.component.css"],
+    selector: "ce-change-password",
+})
+export class ChangePasswordComponent { }
