e20ba2b2faf4f9d864788fbdef1f37d460af0e4f
Quinntyne Brown <quinntynebrown@gmail.com>
2017-08-21 06:14:57 -0400

    change password

--- Features/Users/ChangePasswordCommand.cs
+++ Features/Users/ChangePasswordCommand.cs
@@ -1,12 +1,11 @@
-using MediatR;
 using IdentityService.Data;
 using IdentityService.Features.Core;
+using IdentityService.Features.Security;
+using MediatR;
 using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Linq;
 using System.Data.Entity;
-using IdentityService.Features.Security;
 
 namespace IdentityService.Features.Users
 {
@@ -19,42 +18,53 @@ namespace IdentityService.Features.Users
             public string ConfirmPassword { get; set; }
 
             public UserApiModel User { get; set; }
+
+            public Guid CorrelationId { get; set; }
         }
 
         public class Response { }
 
         public class Handler : IAsyncRequestHandler<Request, Response>
         {
-            public Handler(IdentityServiceContext context, IEventBus bus, IEncryptionService encryptionService)
+            public Handler(IIdentityServiceContext context, IEventBus bus, IEncryptionService encryptionService, IChangePasswordCommandValidator validator)
             {
                 _context = context;
                 _encryptionService = encryptionService;
                 _bus = bus;
+                _validator = validator;
             }
 
             public async Task<Response> Handle(Request request)
             {
-                if (request.ConfirmPassword != request.Password)
-                    throw new Exception();
-
-                var user = await _context.Users
-                    .Include(x => x.Tenant)
-                    .SingleAsync(x => x.Id == request.User.Id);
-
-                user.Password = _encryptionService.TransformPassword(request.Password);
-
-                await _context.SaveChangesAsync();
-
-                return new Response()
+                try
                 {
+                    var results = _validator.Validate(request);
 
-                };
+                    if (!results.IsValid)
+                        throw new Exception(results.Errors.First().ErrorMessage);
+
+                    var user = await _context.Users
+                        .Include(x => x.Tenant)
+                        .SingleAsync(x => x.Id == request.User.Id);
+
+                    user.Password = _encryptionService.TransformPassword(request.Password);
+
+                    await _context.SaveChangesAsync();
+
+                    return new Response()
+                    {
+
+                    };
+                }
+                catch (Exception exception) {
+                    throw exception;
+                }
             }
 
-            private readonly IdentityServiceContext _context;
+            private readonly IIdentityServiceContext _context;
             private readonly IEncryptionService _encryptionService;
             private readonly IEventBus _bus;
+            private readonly IChangePasswordCommandValidator _validator;
         }
-
     }
 }
--- Features/Users/ChangePasswordCommandValidator.cs
+++ Features/Users/ChangePasswordCommandValidator.cs
@@ -16,8 +16,8 @@ namespace IdentityService.Features.Users
                 .WithMessage("Password is required");
 
             RuleFor(x => x.Password)
-                .MinimumLength(8)
-                .WithMessage("Password must be at least 8 characters long");
+                .MinimumLength(6)
+                .WithMessage("Password must be at least 6 characters long");
 
             RuleFor(x => x.ConfirmPassword)
             .Equal(x => x.Password)
